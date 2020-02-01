eda8b9871ddf42fc15619ac175eea15d6e42a5f2
Edwin Bonilla <edwinbonilla@gmail.com>
2020-02-01 22:12:39 +1100

    attacked expts

---
+++ submit_attacked_ggp_runs_from_file_cluster.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH --mem=8GB
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --error=/scratch1/bon136/slurm-%A_%a.err
+#SBATCH --output=/scratch1/bon136/slurm-%A_%a.out
+#
+# Submit no graph experiments to cluster (a feature-based prior is constructed )
+#
+# submit many jobs as an array of jobs
+# use e.g. sbatch -a 0-999 submit_all_noisy_experiments_bracewell.sh
+# where 0-999 are the range of the indices of the jobs
+#
+# GPUs not requested above since the environment was installed with tensorflow cpu
+
+module load cuda/10.0.130
+module load cudnn/v7.5.0-cuda92
+source ~/graphGP-env/bin/activate
+
+IFS=$'\n' read -d '' -r -a lines < runs-attacked-ggp.txt
+
+# Submit job
+if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
+then
+    i=$SLURM_ARRAY_TASK_ID
+    echo ${lines[i]}
+    eval "${lines[i]}"
+else
+    echo "Error: Missing array index as SLURM_ARRAY_TASK_ID"
+fi
+
+
+
